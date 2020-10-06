af9536ff5c8ac614cb58440201d04aec696aeb71
Ethan <Ethan@DESKTOP-78IED7R>
2020-10-05 23:08:23 -0700

    Filled in functions, added Main.java as a testing driver

--- CSE360A1/src/cse360assignment02/AddingMachine.java
+++ CSE360A1/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String memory;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    memory = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
+	  memory = memory + " + " + value;
+	  return;
   }
 
   public void subtract (int value) {
+	  total = total - value;
+	  memory = memory + " - " + value;
+	  return;
   }
 
   public String toString () {
-    return "";
+    return memory;
   }
 
   public void clear() {
+	  memory = "0";
+	  return;
   }
 }
